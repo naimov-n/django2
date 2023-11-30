a341352ccf4d31d88c180b17e4f69f3234d31042
naimov-n <nnnaimov@gmail.com>
2023-11-30 18:41:50 +0500

    naimjon

--- index/urls.py
+++ index/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from index.views import *
 
 urlpatterns = [
+    path('', index),
     path('news/', news),
 ]
--- index/views.py
+++ index/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+def index(request):
+    return HttpResponse('<h1>Hello Page home page</h1>')
+
+
 def news(request):
     return HttpResponse('<h1>Hello Page News</h1>')
