a05aac220ae7b2e11250e0b6b6d8fea9f73b9f95
codecraftuz <ravshanabduvaliyev077@gmail.com>
2023-12-02 05:45:09 -0800

    ravshan

--- django1/settings.py
+++ django1/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'django1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django1',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
     }
 }
 
